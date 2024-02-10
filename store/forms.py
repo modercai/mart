1b90e5c73fc88ec5e886e2cfb50ed96ab9fd5f3b
friday malate <fridaymalate@booknowzambia.com>
2024-02-10 11:48:23 +0200

    half way done vendor admin page

--- store/forms.py
+++ store/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from . models import Product
+from .models import Product
 
 class ProductForm(forms.ModelForm):
-    
     class Meta:
         model = Product
-        fields = ("category","title","description","image","price",)
+        fields = ("category", "title", "description", "image", "price")
+        widgets = {
+            'category': forms.Select(attrs={'class': 'form-control mb-3', 'style': 'background-color: #F0F0F0; font-size: 1.25rem;'}),
+            'title': forms.TextInput(attrs={'class': 'form-control mb-3', 'style': 'background-color: #F0F0F0; font-size: 1.25rem;'}),
+            'description': forms.Textarea(attrs={'class': 'form-control mb-3', 'rows': 10, 'style': 'background-color: #F0F0F0; font-size: 1.25rem;'}),
+            'image': forms.FileInput(attrs={'class': 'form-control-file mb-3', 'style': 'background-color: #F0F0F0; font-size: 1.25rem;'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control mb-3', 'style': 'background-color: #F0F0F0; font-size: 1.25rem;'}),
+        }
+
+
